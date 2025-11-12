13d734ba73af5c44a70d44b24e145b5ba703847e
Anna Hayman <annahay@amazon.com>
2025-11-12 17:54:56 -0500

    Adding static rate limiting

--- rust-runtime/aws-smithy-runtime/src/client/retries/strategy/standard.rs
+++ rust-runtime/aws-smithy-runtime/src/client/retries/strategy/standard.rs
@@ -313,7 +313,7 @@ fn check_rate_limiter_for_delay(
     None
 }
 
-pub(super) fn calculate_exponential_backoff(
+pub(crate) fn calculate_exponential_backoff(
     base: f64,
     initial_backoff: f64,
     retry_attempts: u32,
